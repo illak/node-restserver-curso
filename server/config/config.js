c558f5ad25d2bbde72e21a2ba31828466e513d2e
Illak <i.zapata1989@gmail.com>
2020-01-08 12:56:57 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -4,3 +4,22 @@
 //   PUERTO
 // =============================
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =============================
+//   ENTORNO
+// =============================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// =============================
+//   Base de Datos
+// =============================
+let urlDB;
+
+// if( process.env.NODE_ENV === 'dev' ){
+//     urlDB = 'mongodb://localhost:27017/cafe';
+// } else {
+     urlDB = 'mongodb+srv://illak:euXfuNenlRNTfe1Z@cluster0-cbdtu.gcp.mongodb.net/cafe?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true';
+// }
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -12,40 +15,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-});
+app.use( require('./routes/usuario') );
+   
+   
+mongoose.connect(process.env.URLDB,
+        { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
+        (err) => {
 
-app.post('/usuario', function (req, res) {
+        if (err) throw err;
 
-    let body = req.body;
-
-    if( body.nombre === undefined ){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-    } else {
-        res.json({
-            body
-        });
+        console.log('Base de datos ONLINE!');
     }
-});
-
-app.put('/usuario/:id', function (req, res) {
-    
-    let id = req.params.id;
-
-    res.json({
-        id
-    })
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-});
-   
-   
+);
    
  
 app.listen(process.env.PORT, () => {
